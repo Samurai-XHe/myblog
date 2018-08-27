21d3695fd92fa6ea9137abda31ebd803804a8c03
Samurai-XHe <847834358@qq.com>
2018-08-27 09:20:57 +0800

    demo2更新

--- xnhoutai/urls.py
+++ xnhoutai/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('',generic.TemplateView.as_view(template_name='xnhoutai/home.html'),name='home'),
     path('user_manage/',generic.TemplateView.as_view(template_name='xnhoutai/user_manage.html'),name='user_manage'),
     path('user_serch/',generic.TemplateView.as_view(template_name='xnhoutai/user_serch.html'),name='user_serch'),
+    path('context_manage/',generic.TemplateView.as_view(template_name='xnhoutai/context_manage.html'),name='context_manage'),
 ]
