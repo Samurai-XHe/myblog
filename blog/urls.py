bfe3e31649620e372c2d4c35b963cd750d308219
Samurai-XHe <847834358@qq.com>
2018-10-25 17:33:17 +0800

    更换主题

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from blog import views
+from blog.views import BlogListView, BlogWithTypeView, BlogWithDate, BlogDetailView
 
 
 urlpatterns = [
-    path('', views.blog_list, name='blog_list'),
-    path('<int:pk>/', views.blog_detail, name='blog_detail'),
-    path('type/<int:blog_type_pk>/', views.blogs_with_type, name='blogs_with_type'),
-    path('date/<int:year>/<int:month>/', views.blogs_with_date, name='blogs_with_date')
+    path('', BlogListView.as_view(), name='blog_list'),
+    path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('type/<int:blog_type_pk>/', BlogWithTypeView.as_view(), name='blogs_with_type'),
+    path('date/<int:year>/<int:month>/', BlogWithDate.as_view(), name='blogs_with_date')
 ]
